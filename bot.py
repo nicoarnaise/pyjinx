3adcb58568eb5982f4a1fcfd33e13a0bf4472d92
nicoarnaise <nicolas.arnaise@telecom-st-etienne.fr>
2018-07-06 18:21:26 +0200

    Test audio player. attempt2

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import discord
-import asyncio
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
+    if not discord.opus.is_loaded():
+        discord.opus.load_opus()
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
@@ -16,7 +17,7 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('!test'):
         voice = await client.join_voice_channel(message.author.voice.voice_channel)
-        player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=d62TYemN6MQ')
+        player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=xfUa4IVRZFI')
         player.start()
 
 client.run('Mjk3MTI3MzM4MTg4MDc5MTA1.Daf5HQ.Bj_eHukXITK77SgnhBK4dF24lBo')
