1754e97098d59aa398bce85769a75573b555549f
root <root@vps438617.ovh.net>
2018-07-22 23:43:43 +0200

    First yt player

--- bot.py
+++ bot.py
@@ -1,23 +1,48 @@
-import discord
+from keys.config import PARAMS
 
-client = discord.Client()
+import youtube_dl
+import asyncio
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='-')
 
 
 @client.event
 async def on_ready():
     if not discord.opus.is_loaded():
-        discord.opus.load_opus("/usr/local/lib/")
+        discord.opus.load_opus("/usr/local/lib/libopus.so")
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
+@client.command(pass_context=True)
+async def yt(ctx, url):
+    author = ctx.message.author
+    voice = await author.voice.channel.connect()
+    opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(opts) as ydl:
+        song_info = ydl.extract_info(url, download=False)
+        audio = discord.FFmpegPCMAudio(song_info['url'])
+        voice.play(audio)
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        voice = await client.join_voice_channel(message.author.voice.voice_channel)
-        player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=xfUa4IVRZFI')
-        player.start()
+@client.command(pass_context=True)
+async def leave(ctx):
+    for voice in client.voice_clients:
+        if voice.guild == ctx.message.guild:
+            await voice.disconnect()
 
-client.run('Mjk3MTI3MzM4MTg4MDc5MTA1.Daf5HQ.Bj_eHukXITK77SgnhBK4dF24lBo')
+async def run():
+    await client.start(PARAMS["discord"]["jinx"])
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
+
