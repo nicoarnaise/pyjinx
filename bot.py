1d253cbd56a317d7690506582ae43c6476fbf5f4
root <root@vps438617.ovh.net>
2019-02-09 02:10:19 +0100

    [v0.1] Bot can now play nicely musics

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from keys.config import PARAMS
 import discord
 from discord.ext import commands
 from player import *
+import asyncio
 
 loop = asyncio.get_event_loop()
 client = commands.Bot(command_prefix='-', loop=loop, activity=discord.Game("Im a DJ again <3"))
@@ -34,10 +35,19 @@ async def leave(ctx):
 
 
 @client.command(pass_context=True)
+async def chan(ctx):
+    youtube_player.set_show_chan(ctx.guild.id, show_chan_id=ctx.message.channel.id)
+    await ctx.message.channel.send("Chan set as default.\nFor playing music, please do -yt <youtube_link> :heart:", delete_after=15)
+    await ctx.message.delete()
+
+
+@client.command(pass_context=True)
 async def yt(ctx, *, url):
     if not YoutubePlayer.is_youtube_link(url):
-        await youtube_player.search_video(ctx.message, url, False)
+        print("searching ", url)
+        await youtube_player.search_video(ctx, url, False)
     else:
+        print("playing ", url)
         await youtube_player.add_to_playlist(ctx, url, False)
     await ctx.message.delete()
 
@@ -45,7 +55,7 @@ async def yt(ctx, *, url):
 @client.command(pass_context=True)
 async def ytp(ctx, *, url):
     if not YoutubePlayer.is_youtube_link(url):
-        await youtube_player.search_video(ctx.message, url, True)
+        await youtube_player.search_video(ctx, url, True)
     else:
         await youtube_player.add_to_playlist(ctx, url, True)
     await ctx.message.delete()
@@ -58,81 +68,100 @@ async def on_command_error(ctx, error):
 
 @client.event
 async def on_reaction_add(reaction, user):
-    if not reaction.me:
+    if reaction.count > 1 and reaction.me:
         guild_id = reaction.message.guild.id
         delete = True
-        if reaction.message.id in youtube_player.servers[guild_id]["search_map"]:
-            if reaction == discord.Reaction(emoji=ONE):
-                url = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["urls"][0]
-                playlist = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["playlist"]
-                await youtube_player.add_to_playlist(youtube_player.ctx[guild_id], url, playlist)
+        if reaction.message.id in youtube_player.ctx:
+            context = youtube_player.ctx[reaction.message.id]
+            req_mess_id = context.message.id
+            print(req_mess_id)
+            if reaction.emoji == ONE:
+                url = youtube_player.servers[guild_id]["search_map"][req_mess_id]["urls"][0]
+                playlist = youtube_player.servers[guild_id]["search_map"][req_mess_id]["playlist"]
+                await youtube_player.add_to_playlist(context, url, playlist)
                 await reaction.message.delete()
                 delete=False
-            if reaction == discord.Reaction(emoji=TWO):
-                url = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["urls"][1]
-                playlist = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["playlist"]
-                await youtube_player.add_to_playlist(youtube_player.ctx[guild_id], url, playlist)
+            if reaction.emoji == TWO:
+                url = youtube_player.servers[guild_id]["search_map"][req_mess_id]["urls"][1]
+                playlist = youtube_player.servers[guild_id]["search_map"][req_mess_id]["playlist"]
+                await youtube_player.add_to_playlist(context, url, playlist)
                 await reaction.message.delete()
-                delete = False
-            if reaction == discord.Reaction(emoji=THREE):
-                url = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["urls"][2]
-                playlist = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["playlist"]
-                await youtube_player.add_to_playlist(youtube_player.ctx[guild_id], url, playlist)
+                delete=False
+            if reaction.emoji == THREE:
+                url = youtube_player.servers[guild_id]["search_map"][req_mess_id]["urls"][2]
+                playlist = youtube_player.servers[guild_id]["search_map"][req_mess_id]["playlist"]
+                await youtube_player.add_to_playlist(context, url, playlist)
                 await reaction.message.delete()
-                delete = False
-            if reaction == discord.Reaction(emoji=FOUR):
-                url = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["urls"][3]
-                playlist = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["playlist"]
-                await youtube_player.add_to_playlist(youtube_player.ctx[guild_id], url, playlist)
+                delete=False
+            if reaction.emoji == FOUR:
+                url = youtube_player.servers[guild_id]["search_map"][req_mess_id]["urls"][3]
+                playlist = youtube_player.servers[guild_id]["search_map"][req_mess_id]["playlist"]
+                await youtube_player.add_to_playlist(context, url, playlist)
                 await reaction.message.delete()
-                delete = False
-            if reaction == discord.Reaction(emoji=FIVE):
-                url = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["urls"][4]
-                playlist = youtube_player.servers[guild_id]["search_map"][reaction.message.id]["playlist"]
-                await youtube_player.add_to_playlist(youtube_player.ctx[guild_id], url, playlist)
+                delete=False
+            if reaction.emoji == FIVE:
+                url = youtube_player.servers[guild_id]["search_map"][req_mess_id]["urls"][0]
+                playlist = youtube_player.servers[guild_id]["search_map"][req_mess_id]["playlist"]
+                await youtube_player.add_to_playlist(context, url, playlist)
                 await reaction.message.delete()
-                delete = False
-            if reaction == discord.Reaction(emoji=NPAGEBTN):
-                msg = youtube_player.servers[guild_id]["search_map"][reaction.message.id]
+                delete=False
+            if reaction.emoji == NPAGEBTN:
+                msg = youtube_player.servers[guild_id]["search_map"][req_mess_id]
                 suffix = msg['suffix']
                 playlist = msg['playlist']
                 token = msg['next']
-                await youtube_player.next_page(msg, suffix, playlist, token)
-            if reaction == discord.Reaction(emoji=PPAGEBTN):
-                msg = youtube_player.servers[guild_id]["search_map"][reaction.message.id]
+                await youtube_player.next_page(context, suffix, playlist, token)
+            if reaction.emoji == PPAGEBTN:
+                msg = youtube_player.servers[guild_id]["search_map"][req_mess_id]
                 suffix = msg['suffix']
                 playlist = msg['playlist']
                 token = msg['prev']
-                await youtube_player.next_page(msg, suffix, playlist, token)
-            if reaction == discord.Reaction(emoji=CANCEL):
+                await youtube_player.next_page(context, suffix, playlist, token)
+            if reaction.emoji == CANCEL:
                 await reaction.message.delete()
                 delete = False
 
-        if 'jinxMessage' in youtube_player.servers[guild_id] and reaction.message.id == youtube_player.servers[guild_id]['jinxMessage']:
-            if reaction == discord.Reaction(emoji=NEXTBTN):
+        if 'jinxMessage' in youtube_player.servers[guild_id] and reaction.message.id == youtube_player.servers[guild_id]['jinxMessage'].id:
+            print('playMessage')
+            if reaction.emoji == NEXTBTN:
+                print('next')
                 youtube_player.current[guild_id] += 1
+                youtube_player.voice[guild_id].pause()
                 await youtube_player.play(youtube_player.voice[guild_id], guild_id)
-            if reaction == discord.Reaction(emoji=PREVBTN):
+            if reaction.emoji == PREVBTN:
+                print('prev')
                 youtube_player.current[guild_id] -= 1
+                if youtube_player.current[guild_id] < 0:
+                    youtube_player.current[guild_id] = 0
+                youtube_player.voice[guild_id].pause()
                 await youtube_player.play(youtube_player.voice[guild_id], guild_id)
-            if reaction == discord.Reaction(emoji=REPLAYBTN):
+            if reaction.emoji == REPLAYBTN:
+                print('replay')
+                youtube_player.voice[guild_id].pause()
                 await youtube_player.play(youtube_player.voice[guild_id], guild_id)
-            if reaction == discord.Reaction(emoji=PAUSEBTN):
+            if reaction.emoji == PAUSEBTN:
+                print('pause')
                 if 'paused' not in youtube_player.servers[guild_id] or not youtube_player.servers[guild_id]['paused']:
                     youtube_player.voice[guild_id].pause()
                     youtube_player.servers[guild_id]['paused'] = True
                 else:
                     youtube_player.voice[guild_id].resume()
-            if reaction == discord.Reaction(emoji=STOPBTN):
+                    youtube_player.servers[guild_id]['paused'] = False
+            if reaction.emoji == STOPBTN:
                 print("stop asked, leaving ...")
                 # delete jinx youtube player message
                 await youtube_player.servers[guild_id]['jinxMessage'].delete()
                 # make jinx leave voice chat
                 await youtube_player.ctx[guild_id].invoke(leave)
                 delete = False
-            if reaction == discord.Reaction(emoji=LOOPBTN):
-                youtube_player.servers[guild_id]['infinite'] = not youtube_player.servers[guild_id]['infinite']
-                delete = False
+            if reaction.emoji == LOOPBTN:
+                print('loop')
+                if 'infinite' not in youtube_player.servers[guild_id]:
+                    youtube_player.servers[guild_id]['infinite'] = True
+                else:
+                    youtube_player.servers[guild_id]['infinite'] = not youtube_player.servers[guild_id]['infinite']
+                await youtube_player.edit_embed(guild_id)
+
         if delete:
             await reaction.message.remove_reaction(reaction, user)
 
--- player.py
+++ player.py
@@ -1,5 +1,8 @@
 from keys.config import PARAMS
-from apiclient.discovery import build
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 from discord import Embed
 import discord
 import re
@@ -30,10 +33,11 @@ YTPL = 'http://www.youtube.com/playlist?list='
 
 
 def _get_first_permission_chan(guild):
-    for chan in guild.text_channels:
-        if chan.permission_for(guild.me).send_messages:
-            return chan
-    return None
+    #FIXME I dn't even know why it crashes silently -_-
+    #for chan in guild.text_channels::
+    #    if chan.permission_for(guild.me).send_messages:
+    #        return chan
+    return guild.text_channels[0]
 
 
 class YoutubePlayer:
@@ -50,8 +54,10 @@ class YoutubePlayer:
 
     @staticmethod
     def is_youtube_link(link):
-        yt_pattern = re.compile("(http://|https://)(www\.)?(youtu)(.*)")
-        return yt_pattern.match(link)
+        if 'http' in link and (('youtube' in link and ('v=' in link or 'list=' in link)) or 'youtu.be' in link):
+            return True
+        else:
+            return False
 
     def _get_guild_by_id(self, guild_id):
         for guild in self.client.guilds:
@@ -63,48 +69,51 @@ class YoutubePlayer:
         if guild_id not in self.servers:
             self.servers[guild_id] = {}
         guild = self._get_guild_by_id(guild_id)
+        print(guild.text_channels)
         if show_chan_id is None:
             show_chan = _get_first_permission_chan(guild)
             show_chan_id = show_chan.id
         else:
             show_chan = guild.get_channel(show_chan_id)
+        print(show_chan)
         self.servers[guild_id]["id"] = show_chan_id
         self.servers[guild_id]["chan"] = show_chan
 
-    async def search_video(self, ctx, msg, suffix, playlist):
-        guild_id = str(msg.guild)
-        self.ctx[guild_id] = ctx
+    async def search_video(self, ctx, suffix, playlist):
+        print('in search')
+        guild_id = ctx.guild.id
+        print(guild_id)
+
         if guild_id not in self.servers:
             self.servers[guild_id] = {}
-        if "list_to_play" not in self.servers[guild_id]:
-            self.servers[guild_id]["list_to_play"] = []
         if "search_map" not in self.servers[guild_id]:
             self.servers[guild_id]["search_map"] = {}
-        await self.next_page(msg, suffix, playlist)
+        await self.next_page(ctx, suffix, playlist)
 
-    async def next_page(self, msg, suffix, playlist, token=None):
+    async def next_page(self, ctx, suffix, playlist, token=None):
         if token is None:
-            response = self.youtube.search.list(
+            print('next_page init search')
+            response = self.youtube.search().list(
                 part='snippet',
                 type='playlist' if playlist else 'video',
                 q=suffix
             ).execute()
         else:
-            response = self.youtube.search.list(
+            response = self.youtube.search().list(
                 part='snippet',
                 type='playlist' if playlist else 'video',
                 q=suffix,
                 pageToken=token
             ).execute()
-        await self.show_result_page(msg, response, suffix, playlist)
+        print(response)
+        await self.show_result_page(ctx, response, suffix, playlist)
 
-    async def show_result_page(self, msg, response, suffix, playlist):
-        guild_id = str(msg.guild)
-        embed = Embed()
-        embed.title = 'Search results'
-        embed.colour = 1669525
+    async def show_result_page(self, ctx, response, suffix, playlist):
+        print('showing message')
+        guild_id = ctx.guild.id
+        msg_id = ctx.message.id
+        embed = Embed(title='Search results', color=1669525, description='searching `%s`'%suffix)
         embed.set_thumbnail(url='https://www.youtube.com/yts/img/yt_1200-vfl4C3T0K.png')
-        embed.description = 'searching `%s`' % suffix
         embed.set_footer(text='Make your choice by reacting, change pages with %s or %s and cancel with %s'
                               % (PPAGEBTN, NPAGEBTN, CANCEL))
         urls = []
@@ -114,14 +123,22 @@ class YoutubePlayer:
                 value='desc: `%s`' % (video["snippet"]["description"])
             )
             urls.append((YTPL + video["id"]["playlistId"]) if playlist else (YTLINK + video["id"]["videoId"]))
-        if msg.id in self.servers[guild_id]["search_map"]:
-            await msg.edit(embed=embed)
-            self.servers[guild_id]["search_map"][msg.id]["next"] = response.get("nextPageToken", "")
-            self.servers[guild_id]["search_map"][msg.id]["prev"] = response.get("prevPageToken", "")
-            self.servers[guild_id]["search_map"][msg.id]["urls"] = urls
-            self.servers[guild_id]["search_map"][msg.id]["playlist"] = playlist
+        print(urls)
+
+        if guild_id not in self.servers:
+            self.servers[guild_id] = {}
+        if "search_map" not in self.servers[guild_id]:
+            self.servers[guild_id]["search_map"] = {}
+        if msg_id in self.servers[guild_id]["search_map"]:
+            print('editing message')
+            await self.servers[guild_id]["search_map"][msg_id]["message"].edit(embed=embed)
+            self.servers[guild_id]["search_map"][msg_id]["next"] = response.get("nextPageToken", "")
+            self.servers[guild_id]["search_map"][msg_id]["prev"] = response.get("prevPageToken", "")
+            self.servers[guild_id]["search_map"][msg_id]["urls"] = urls
+            self.servers[guild_id]["search_map"][msg_id]["playlist"] = playlist
         else:
-            message = await msg.channel.send(embed=embed)
+            print('sending message')
+            message = await ctx.message.channel.send(embed=embed)
             await message.add_reaction(PPAGEBTN)
             await message.add_reaction(ONE)
             await message.add_reaction(TWO)
@@ -130,73 +147,96 @@ class YoutubePlayer:
             await message.add_reaction(FIVE)
             await message.add_reaction(NPAGEBTN)
             await message.add_reaction(CANCEL)
-            self.servers[guild_id]["search_map"][message.id] = {
-                'msg': msg,
+            self.ctx[message.id] = ctx
+            print(msg_id)
+            self.servers[guild_id]["search_map"][msg_id] = {
                 'urls': urls,
                 'suffix': suffix,
                 'playlist': playlist,
                 'next': response.get("nextPageToken", ""),
-                'prev': response.get("prevPageToken", "")
+                'prev': response.get("prevPageToken", ""),
+                'message': message
             }
 
     async def add_to_playlist(self, ctx, request, playlist):
         self.ctx[ctx.guild.id] = ctx
+        print(request)
         if self.is_youtube_link(request):
+            print("adding to playlist ", request)
             videos_to_add = []
             if playlist:
-                playlist_id = re.search("([^\/]+\/\/[^\/]+\/(watch\?)?)(.*)list=([^\&\?]+)(.*)", request).group(3)
+                playlist_id = re.search("([^\/]+\/\/[^\/]+\/(watch\?)?)(.*)list=([^\&\?]+)(.*)", request).group(4)
+                req_nb_vid = self.youtube.playlistItems().list(
+                    part='snippet',
+                    playlistId=playlist_id
+                ).execute()
                 response = self.youtube.playlistItems().list(
                     part='snippet',
-                    id=playlist_id
-                )
-                for item in response.get('items'):
+                    maxResults=req_nb_vid["pageInfo"]["totalResults"],
+                    playlistId=playlist_id
+                ).execute()
+                print(response)
+                for item in response['items']:
                     video = item["snippet"]
-                    videos_to_add.append({'url': YTLINK + video["contentDetails"]["videoId"],
+                    if 'medium' in video['thumbnails']:
+                        videos_to_add.append({'url': YTLINK + video["resourceId"]["videoId"],
+                                          'thumbnail': video["thumbnails"]["medium"]["url"],
+                                          'title': video["title"]})
+                    else:
+                        videos_to_add.append({'url': YTLINK + video["resourceId"]["videoId"],
                                           'thumbnail': video["thumbnails"]["default"]["url"],
-                                          'tittle': video["tittle"]})
+                                          'title': video["title"]})
             else:
                 if 'youtu.be' in request:
-                    video_id = re.search(".*/(.*)", request).group(0)
+                    video_id = re.search(".*/(.*)", request).group(1)
                 else:
-                    video_id = re.search("([^\/]+\/\/[^\/]+\/(watch\?)?)(.*)v=([^\&\?]+)(.*)", request).group(3)
-                response = self.youtube.videos().list(
-                    part='snippet',
-                    id=video_id
-                ).execute()
-                video = response.get('items')[0]["snippet"]
-                videos_to_add.append({'url': YTLINK+video_id,
-                                      'thumbnail': video["thumbnails"]["default"]["url"],
-                                      'tittle': video["tittle"]})
-
+                    video_id = re.search("([^\/]+\/\/[^\/]+\/(watch\?)?)(.*)v=([^\&\?]+)(.*)", request).group(4)
+                print(video_id)
+                try:
+                    response = self.youtube.videos().list(
+                        part='snippet',
+                        id=video_id
+                    ).execute()
+                except HttpError as e:
+                    print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+                video = response['items'][0]["snippet"]
+                videos_to_add.append({'url': YTLINK+video_id, 'thumbnail': video["thumbnails"]["default"]["url"], 'title': video["title"]})
+            print("adding videos %s" % videos_to_add)
             if ctx.guild.id not in self.playlist:
                 self.playlist[ctx.guild.id] = []
             for video in videos_to_add:
                 self.playlist[ctx.guild.id].append(video)
-            await self.edit_embed(ctx.guild.id)
-            if not self.is_playing[ctx.guild.id]:
+            if ctx.guild.id not in self.is_playing or not self.is_playing[ctx.guild.id]:
                 author = ctx.message.author
                 voice = await author.voice.channel.connect()
                 self.voice[ctx.guild.id] = voice
                 await self.play(voice, ctx.guild.id)
+            else:
+                await self.edit_embed(ctx.guild.id)
+
 
     async def play(self, voice, guild_id):
         ctx = self.ctx[guild_id]
-        if ctx.guild.id not in self.current:
-            self.current[ctx.guild.id] = 0
+        if guild_id not in self.current:
+            self.current[guild_id] = 0
 
         opts = {
             'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
+            'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+            'nocheckcertificate': True,
+            'restrictfilenames': True,
+            'source_address': '0.0.0.0',
+            #'postprocessors': [{
+            #    'key': 'FFmpegExtractAudio',
+            #    'preferredcodec': 'mp3',
+            #    'preferredquality': '192',
+            #}],
         }
 
         def my_after(error):
             if error is not None:
-                self.servers[ctx.guild.id]["chan"].send(error)
-            self.current[ctx.guild.id] += 1
+                asyncio.run_couroutine_threadsafe(self.servers[guild_id]["chan"].send(error), self.client.loop)
+            self.current[guild_id] += 1
             print(self.current[guild_id], len(self.playlist[guild_id]))
             infinite = self.servers[guild_id]['infinite'] if 'infinite' in self.servers[guild_id] else False
             if self.current[guild_id] >= len(self.playlist[guild_id]) and not infinite:
@@ -205,32 +245,44 @@ class YoutubePlayer:
                 asyncio.run_coroutine_threadsafe(self.servers[guild_id]['jinxMessage'].delete(), self.client.loop)
                 # make jinx leave voice chat
                 asyncio.run_coroutine_threadsafe(voice.disconnect(), self.client.loop)
+                self.current[guild_id] = 0
+                self.playlist[guild_id] = []
+                self.is_playing[guild_id] = False
             else:
                 if infinite:
-                    self.current[ctx.guild.id] = 0
+                    self.current[guild_id] = 0
                 asyncio.run_coroutine_threadsafe(self.play(voice, guild_id), self.client.loop)
 
         self.is_playing[guild_id] = True
+        print("editing embed for guild %s" % guild_id)
         await self.edit_embed(guild_id)
         with youtube_dl.YoutubeDL(opts) as ydl:
-            song_info = ydl.extract_info(self.playlist[ctx.guild.id][self.current[guild_id]]['url'], download=False)
-            audio = discord.FFmpegPCMAudio(song_info['url'])
+            song_info = ydl.extract_info(self.playlist[ctx.guild.id][self.current[guild_id]]['url'], download=True)
+            audio = discord.FFmpegPCMAudio(ydl.prepare_filename(song_info))
             voice.play(audio, after=my_after)
 
     async def edit_embed(self, guild_id):
-        embed = Embed()
-        embed.title = self.playlist[guild_id][self.current[guild_id]]['title']
-        embed.colour = 12345678
+        embed = Embed(
+                      title=self.playlist[guild_id][self.current[guild_id]]['title'],
+                      color=12345678,
+                      description='Video n° `' + str(self.current[guild_id] + 1) + '` / `' + str(len(self.playlist[guild_id])) + '`'
+        )
         embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/297127338188079105/4b7b6e74a835176401c0c3affd5ce8d6.jpg?size=1024')
         embed.set_image(url=self.playlist[guild_id][self.current[guild_id]]['thumbnail'])
-        embed.description = 'Video n° `' + str(self.current[guild_id] + 1) + '` / `' + str(len(self.playlist[guild_id])) + '`'
-
+        infinite = self.servers[guild_id]['infinite'] if 'infinite' in self.servers[guild_id] else False
+        embed.set_footer(text="looping over playlist : %s" % infinite)
+        print("embed created")
+        if guild_id not in self.servers:
+            self.servers[guild_id] = {}
         if 'chan' not in self.servers[guild_id]:
+            print("setting show chan")
             self.set_show_chan(guild_id)
         try:
             await self.servers[guild_id]['jinxMessage'].edit(embed=embed)
-        except Exception:
+        except:
+            print("sending jinxMessage")
             message = await self.servers[guild_id]['chan'].send(embed=embed)
+            print(str(message))
             await message.add_reaction(LOOPBTN)
             await message.add_reaction(PREVBTN)
             await message.add_reaction(PAUSEBTN)
